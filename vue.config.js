9c089dc002117fef709ea2a76d34b2d2fc594d3c
mpakholska <marta.pakholska.work@gmail.com>
2025-07-18 11:52:17 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,26 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './', // ✅ Set to relative path for static hosts like Netlify
-
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  productionSourceMap: false,
+  
+  // Copy files to dist folder
+  chainWebpack: config => {
+    config.plugin('copy').tap(([options]) => {
+      options[0].patterns.push({
+        from: 'public/WebMap30-04.xlsx',
+        to: 'WebMap30-04.xlsx'
+      })
+      options[0].patterns.push({
+        from: 'public/countries.json',
+        to: 'countries.json'
+      })
+      return [options]
+    })
+  },
+  
   pluginOptions: {
     vuetify: {}
   }
