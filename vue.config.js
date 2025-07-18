23e4d2bcf412f7cf9897fea3c66ff34450e0e429
mpakholska <marta.pakholska.work@gmail.com>
2025-07-18 11:29:30 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './', // ✅ Set to relative path for static hosts like Netlify
