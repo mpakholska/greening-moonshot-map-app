464d64c1e68d7986cd5ad074a5935f93c644d6f5
mpakholska <marta.pakholska.work@gmail.com>
2025-07-21 12:15:07 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,37 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  
+  // Use relative paths for static deployment
   publicPath: './',
+  
+  // Output configuration
   outputDir: 'dist',
   assetsDir: 'static',
+  
+  // Disable source maps to reduce bundle size
   productionSourceMap: false,
   
+  // Configure build for static deployment
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          }
+        }
+      }
+    }
+  },
+  
   pluginOptions: {
-    vuetify: {}
+    vuetify: {
+      // Placeholder for Vuetify loader options
+    }
   }
 });
